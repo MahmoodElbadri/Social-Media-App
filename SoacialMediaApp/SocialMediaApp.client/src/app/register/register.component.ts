7971c3a9c8368e0bba21ddf2953d4fab91203455
Mahmood Elbadri <mahmoud.elbadry357@gmail.com>
2025-11-08 16:23:56 +0200

    Refactor register form to use reactive forms
    - Replaced template-driven forms with reactive forms for better scalability and control.
    - Added form initialization logic and `FormGroup` with `FormControl` for username, password, and confirm password.
    - Updated template to bind input fields using `formControlName`.
    - Removed unused dropdown and associated logic from the template.

--- SoacialMediaApp/SocialMediaApp.client/src/app/register/register.component.ts
+++ SoacialMediaApp/SocialMediaApp.client/src/app/register/register.component.ts
@@ -1,37 +1,54 @@
-import {Component, EventEmitter, inject, input, Input, Output} from '@angular/core';
-import {FormsModule} from "@angular/forms";
-import {AccountService} from "../_services/account.service";
-import {ToastrService} from "ngx-toastr";
+import { Component, EventEmitter, inject, input, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AccountService } from "../_services/account.service";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
   model: any = {}
   // @Input() userFromHomeComponent: any;
   userFromHomeComponent = input<any>();
   @Output() cancelRegister = new EventEmitter();
   protected accountService = inject(AccountService);
   private toastr = inject(ToastrService);
+  registerForm: FormGroup = new FormGroup({});
+
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl(),
+
+    });
+  }
 
   register() {
-    this.accountService.register(this.model).subscribe({
-      next: () => {
-        this.cancel();
-      },
-      error: (error) => {
-        console.log(error);
-        this.toastr.error(error.error);
-      },
-      complete:()=>{
-        this.cancel();
-        console.log("completed Register");
-      }
-    });
+    console.log(this.registerForm.value);
+    // this.accountService.register(this.model).subscribe({
+    //   next: () => {
+    //     this.cancel();
+    //   },
+    //   error: (error) => {
+    //     console.log(error);
+    //     this.toastr.error(error.error);
+    //   },
+    //   complete: () => {
+    //     this.cancel();
+    //     console.log("completed Register");
+    //   }
+    //});
 
   }
 
