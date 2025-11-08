b0050c870b87233f9862fae1f89e5647cf102a54
Mahmood Elbadri <mahmoud.elbadry357@gmail.com>
2025-11-08 17:17:15 +0200

    Adding Reactive forms validations

--- SoacialMediaApp/SocialMediaApp.client/src/app/register/register.component.ts
+++ SoacialMediaApp/SocialMediaApp.client/src/app/register/register.component.ts
@@ -1,12 +1,13 @@
 import { Component, EventEmitter, inject, input, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from "@angular/forms";
 import { AccountService } from "../_services/account.service";
 import { ToastrService } from "ngx-toastr";
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule, FormsModule],
+  imports: [ReactiveFormsModule, FormsModule, JsonPipe],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
@@ -27,11 +28,23 @@ export class RegisterComponent implements OnInit {
 
   initializeForm() {
     this.registerForm = new FormGroup({
-      username: new FormControl(),
-      password: new FormControl(),
-      confirmPassword: new FormControl(),
-
+      username: new FormControl('', [Validators.required, Validators.maxLength(20), Validators.minLength(4)]),
+      password: new FormControl('', Validators.required),
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')]),
     });
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next: () => {
+        this.registerForm.controls['confirmPassword'].updateValueAndValidity();
+      }
+    })
+  }
+
+  matchValues(matchTo: string):ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value
+       ? null
+       : {notMatching: true} //this name will be used in the template to check for the error keep it in mind
+    }
   }
 
   register() {
